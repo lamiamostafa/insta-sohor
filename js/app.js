73080793fae33cc9cee4cc027e235e443122dba3
lamiamostafa <lamiamostafa791@gmail.com>
2022-03-11 22:08:37 +0600

    bug 4 completed

--- js/app.js
+++ js/app.js
@@ -28,6 +28,7 @@ const reportPost = (id) => {
 };
 
 const displayContent = (text) => {
+  console.log(text);
   return text.length < 30 ? text : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
 };
 
@@ -121,9 +122,9 @@ const createPost = (post) => {
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
-                          ${post.comments?.user}
+                          ${post.comments[0]?.user}
                       </a>
-                      ${post.comments?.text}
+                      ${post.comments[0].text}
                     </small>
                   </div>
                   <span class="post__date-time">30 minutes ago</span>
@@ -154,10 +155,11 @@ const displayLikedPosts = () => {
 
 const displayReportedPosts = () => {
   const reportedPosts = getReportedPosts();
-  reportedPosts.textContent = '';
+  reportedPosts.textContent = "";
   reportedPosts.forEach((post) => {
     const div = createPost(post);
     document.getElementById("reported").appendChild(div);
+
   });
 };
 
